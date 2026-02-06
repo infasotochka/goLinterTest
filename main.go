2fc9489f2d83e7a49da7c471bdfe4e65f9e997be
Denis Frolov <infasotkayakrasotka@gmail.com>
2026-02-06 17:55:03 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Деление на нуль")
+	}
+	return a / b, nil
+}
+
+func main() {
+	fmt.Println(divide(10, 0))
+}
